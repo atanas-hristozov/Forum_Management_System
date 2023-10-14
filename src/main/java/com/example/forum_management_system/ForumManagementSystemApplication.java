94132dc4978b728c6c71179d194744703430f4a1
Atanas Hristozov <64282150+atanas-hristozov@users.noreply.github.com>
2023-10-14 11:38:54 +0300

    Project_Structutre

---
+++ src/main/java/com/example/forum_management_system/ForumManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.forum_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/forum_management_system/ForumManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forum_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
