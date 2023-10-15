7c550ddae308c37e2f6eacd857a1b98c033969d2
Atanas Hristozov <64282150+atanas-hristozov@users.noreply.github.com>
2023-10-15 20:17:04 +0300

    Comment_Controller

---
+++ src/DB/Tables_Data.sql
@@ -0,0 +1,13 @@
+INSERT INTO users values (1, 'Ivan', 'Ivanov', 'ivan@gmail.com', 'Ivan92', '123456', 1, '0888888888');
+INSERT INTO users values (2, 'Georgi', 'Petrov', 'georgi@gmail.com', 'Georgi97', '0000', 1, '0888888887');
+INSERT INTO users values (3, 'Todor', 'Georgiev', 'todor@gmail.com', 'Todor94', '54321', 1, '0888888886');
+INSERT INTO users values (4, 'Marin', 'Zlatkov', 'marin@gmail.com', 'Marin99', '112211', 1, '0888888885');
+INSERT INTO users values (5, 'Gergana', 'Veseliova', 'gergana@gmail.com', 'Gergana91', 'qwe123', 1, '0888888884');
+
+INSERT INTO posts values (1, 'FirstPost', 'This is the first post.', 0, 0, 1, null);
+INSERT INTO posts values (2, 'SecondPost', 'This is the second post.', 0, 0, 2, null);
+INSERT INTO posts values (3, 'ThirdPost', 'This is the third post.', 0, 0, 3, null);
+
+INSERT INTO comments values (1, 'First comment', 3);
+INSERT INTO comments values (2, 'Second comment', 4);
+INSERT INTO comments values (3, 'Third comment', 5);
