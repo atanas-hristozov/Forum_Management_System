fc531673fdbbb5659da353ecc8e5879ed0d4e79f
Atanas Hristozov <64282150+atanas-hristozov@users.noreply.github.com>
2023-10-16 12:14:46 +0300

    Table_Data

--- src/DB/Tables_Data.sql
+++ src/DB/Tables_Data.sql
@@ -4,10 +4,10 @@ INSERT INTO users values (3, 'Todor', 'Georgiev', 'todor@gmail.com', 'Todor94', 
 INSERT INTO users values (4, 'Marin', 'Zlatkov', 'marin@gmail.com', 'Marin99', '112211', 1, '0888888885');
 INSERT INTO users values (5, 'Gergana', 'Veseliova', 'gergana@gmail.com', 'Gergana91', 'qwe123', 1, '0888888884');
 
-INSERT INTO posts values (1, 'FirstPost', 'This is the first post.', 0, 0, 1, null);
-INSERT INTO posts values (2, 'SecondPost', 'This is the second post.', 0, 0, 2, null);
-INSERT INTO posts values (3, 'ThirdPost', 'This is the third post.', 0, 0, 3, null);
+INSERT INTO posts values (1, 'FirstPost', 'This is the first post.', 0, 0, 1);
+INSERT INTO posts values (2, 'SecondPost', 'This is the second post.', 0, 0, 2);
+INSERT INTO posts values (3, 'ThirdPost', 'This is the third post.', 0, 0, 3);
 
-INSERT INTO comments values (1, 'First comment', 3);
-INSERT INTO comments values (2, 'Second comment', 4);
-INSERT INTO comments values (3, 'Third comment', 5);
+INSERT INTO comments values (1, 'First comment', 1, 3);
+INSERT INTO comments values (2, 'Second comment', 2, 4);
+INSERT INTO comments values (3, 'Third comment', 3, 5);
