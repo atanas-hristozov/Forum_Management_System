aa3038217f385b4cffd7a0fdf9b4f053fd6cbe7a
Atanas Hristozov <64282150+atanas-hristozov@users.noreply.github.com>
2023-10-30 14:00:23 +0200

    FrontEnd_Updated_Posts_Functionality

--- src/main/java/com/example/forum_management_system/controllers/mvc/ForumMvcController.java
+++ src/main/java/com/example/forum_management_system/controllers/mvc/ForumMvcController.java
@@ -1,10 +1,16 @@
 package com.example.forum_management_system.controllers.mvc;
 
+import com.example.forum_management_system.models.Post;
+import com.example.forum_management_system.models.PostDto;
 import com.example.forum_management_system.services.PostService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/forum")
@@ -17,7 +23,14 @@ public class ForumMvcController {
 
     @GetMapping
     public String showForumPage(Model model){
-        //model.addAttribute("posts", service.get(post))
+        List<Post> posts = service.get(null,null,null,null);
+        model.addAttribute("posts", posts);
         return "Forum";
     }
+
+    /*@GetMapping
+    public String createPost(@Valid @ModelAttribute PostDto post){
+        //service.create(post);
+        return "redirect:Form";
+    }*/
 }
