e590cd47da8b9b73ca626a5cc6b2a9310e8fee0f
Atanas Hristozov <64282150+atanas-hristozov@users.noreply.github.com>
2023-11-01 12:28:15 +0200

    Create_NewPost

---
+++ src/main/java/com/example/forum_management_system/controllers/mvc/CreatePostMvcController.java
@@ -0,0 +1,72 @@
+package com.example.forum_management_system.controllers.mvc;
+
+import com.example.forum_management_system.exceptions.AuthorizationException;
+import com.example.forum_management_system.exceptions.EntityDuplicateException;
+import com.example.forum_management_system.exceptions.EntityNotFoundException;
+import com.example.forum_management_system.helpers.AuthenticationHelper;
+import com.example.forum_management_system.helpers.PostMapper;
+import com.example.forum_management_system.models.Post;
+import com.example.forum_management_system.models.PostDto;
+import com.example.forum_management_system.models.User;
+import com.example.forum_management_system.services.PostService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/post/new")
+public class CreatePostMvcController {
+    private final PostService postService;
+    private final AuthenticationHelper authenticationHelper;
+    private final PostMapper postMapper;
+
+    public CreatePostMvcController(PostService postService, AuthenticationHelper authenticationHelper, PostMapper postMapper) {
+        this.postService = postService;
+        this.authenticationHelper = authenticationHelper;
+        this.postMapper = postMapper;
+    }
+
+    @ModelAttribute("isAuthenticated")
+    public boolean populateIsAuthenticated(HttpSession session) {
+        return session.getAttribute("currentUser") != null;
+    }
+
+    @GetMapping
+    public String showCreatePage(Model model){
+        model.addAttribute("posts", new PostDto());
+        return "CreateNewPost";
+    }
+
+    @PostMapping
+    public String createPost(@Valid @ModelAttribute("post")PostDto postDto, BindingResult bindingResult, Model model, HttpSession session) {
+        User user;
+        try {
+            user = authenticationHelper.tryGetCurrentUser(session);
+        } catch (AuthorizationException e) {
+            return "redirect:/auth/login";
+        }
+        if (bindingResult.hasErrors()) {
+            return "Forum";
+        }
+        try {
+            Post post = postMapper.fromDto(postDto);
+            postService.create(post,user);
+
+            return "redirect:/Forum";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("statusCode", HttpStatus.NOT_FOUND.getReasonPhrase());
+            model.addAttribute("error", e.getMessage());
+            return "ErrorView";
+        } catch (EntityDuplicateException e) {
+            bindingResult.rejectValue("title", "duplicate_title", e.getMessage());
+            return "Forum";
+        }
+    }
+}
